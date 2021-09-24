f58613f51dcc60cb91b0eacb18eb251e77108791
namhihi <yentth237@gmail.com>
2021-09-25 01:35:00 +0700

    set limit rate query, and body

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import _ from 'lodash';
+import cors from 'cors';
+import redis from 'redis';
+import path from 'path';
+import rateLimit from 'express-rate-limit';
+
 import typeDefs from './schemaGraphql';
 import resolvers from './modules';
 import { envVariable } from './configs';
 import { logger, jwtUtils } from './utils';
 import AppContext from './configs/context';
-import _ from 'lodash';
-import cors from 'cors';
-import redis from 'redis';
-import path from 'path';
 import models from './models';
-
 import { authenticationRouter } from './routers';
 
 const redisClient = redis.createClient();
@@ -27,6 +28,21 @@ export const startServer = async () => {
   // view engine setup
   app.set('views', path.join(process.cwd(), 'views'));
   app.set('view engine', 'ejs');
+
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false, limit: '20mb', parameterLimit: 100 }));
+  app.use(express.static(path.join(process.cwd(), 'public')));
+
+  // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+  // see https://expressjs.com/en/guide/behind-proxies.html
+  app.set('trust proxy', 1); // trust first proxy
+  // set rate limit
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests, please try again later.',
+  }));
 
   const server = new ApolloServer({
     typeDefs,
