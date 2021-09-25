8c8bb244406d15ba18c9f375edef04807c57ad27
namhihi <yentth237@gmail.com>
2021-09-25 11:19:07 +0700

    update some remove redis

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import _ from 'lodash';
 import cors from 'cors';
-import redis from 'redis';
 import path from 'path';
-import rateLimit from 'express-rate-limit';
 
 import typeDefs from './schemaGraphql';
 import resolvers from './modules';
 import { envVariable } from './configs';
 import { logger, jwtUtils } from './utils';
-import AppContext from './configs/context';
 import models from './models';
 import { authenticationRouter } from './routers';
 
-const redisClient = redis.createClient();
 const pathServer = '/api/v1/graphql';
 
 export const startServer = async () => {
@@ -33,16 +29,6 @@ export const startServer = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false, limit: '20mb', parameterLimit: 100 }));
   app.use(express.static(path.join(process.cwd(), 'public')));
-
-  // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-  // see https://expressjs.com/en/guide/behind-proxies.html
-  app.set('trust proxy', 1); // trust first proxy
-  // set rate limit
-  // app.use(rateLimit({
-  //   windowMs: 15 * 60 * 1000, // 15 minutes
-  //   max: 100, // limit each IP to 100 requests per windowMs
-  //   message: 'Too many requests, please try again later.',
-  // }));
 
   const server = new ApolloServer({
     typeDefs,
@@ -64,8 +50,6 @@ export const startServer = async () => {
   await server.start();
   server.applyMiddleware({ app, path: pathServer });
 
-  global.appContext = new AppContext();
-  global.redisClient = redisClient;
   global.logger = logger;
   // seed(); // initialize database
 
--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,7 @@
 import logger from './logger';
 import jwtUtils from './jsonwebtoken';
 import bcryptUtils from './bcrypt';
-import redisUtils from './redis';
 import emailUtils from './email';
 import randomUtils from './random';
 
-export { logger, jwtUtils, bcryptUtils, redisUtils, emailUtils, randomUtils };
+export { logger, jwtUtils, bcryptUtils, emailUtils, randomUtils };
