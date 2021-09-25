7bc233b19a0cd69544294b7829c13120d5dc6a18
namhihi <yentth237@gmail.com>
2021-09-25 14:25:19 +0700

    fix env app

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import express from 'express';
 import _ from 'lodash';
 import cors from 'cors';
 import path from 'path';
-import https from "https";
-import http from "http";
 
 import typeDefs from './schemaGraphql';
 import resolvers from './modules';
@@ -33,17 +31,6 @@ export const startServer = async () => {
   app.use(express.urlencoded({ extended: false, limit: '20mb', parameterLimit: 100 }));
   app.use(express.static(path.join(process.cwd(), 'public')));
 
-  const configurations = {
-    // Note: You may need sudo to run on port 443
-    production: { ssl: true, port: 443, hostname: 'localhost' },
-    development: { ssl: false, port: 4000, hostname: 'localhost' },
-  };
-
-  const environment = process.env.NODE_ENV || 'production';
-  const config = configurations[environment];
-
-
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -67,29 +54,9 @@ export const startServer = async () => {
   global.logger = logger;
   // seed(); // initialize database
 
-
-  // Create the HTTPS or HTTP server, per configuration
-  let httpServer;
-  if (config.ssl) {
-    // Assumes certificates are in a .ssl folder off of the package root.
-    // Make sure these files are secured.
-    httpServer = https.createServer(
-      {
-        key: fs.readFileSync('./ssl/private.key'),
-        cert: fs.readFileSync('./ssl/certificate.crt'),
-        ca: [fs.readFileSync('./ssl/ca_bundle.crt')],
-      },
-      app,
-    );
-  } else {
-    httpServer = http.createServer(app);
-  }
-  await new Promise(resolve => httpServer.listen({ port: envVariable.PORT }, resolve));
-
-  console.log(
-    '🚀 Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${server.graphqlPath}`
-  );
+  app.listen({ port: envVariable.PORT }, () => {
+    logger.info(`🚀 Server ready at http://localhost:${envVariable.PORT}${server.graphqlPath}`);
+  });
 };
 
 startServer();
